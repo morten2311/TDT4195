8a08adf787d644147def152ea3136d7f990af110
Morten <morten23_lundenes@hotmail.com>
2016-10-20 13:16:07 +0200

    much work

--- gloom/shaders/simple.vert
+++ gloom/shaders/simple.vert
@@ -4,13 +4,15 @@ layout (location=0) in vec3 position;
 layout (location=1) in vec4 vertexColor;
 out vec4 color;
 //Matrices
-vec4 newPos;
-mat4x4 	matrix = mat4(1);
+//vec4 newPos;
+//mat4x4 	matrix = mat4(1);
 
+uniform layout(location=3) mat4x4 transform;
 
 void main()
-{	matrix[3][1]=1.5;
-	newPos=matrix*vec4(position,1);
- 	gl_Position = vec4(newPos);
+{	
+	//matrix[3][1]=1;
+	//newPos=matrix*vec4(position,1);
+ 	gl_Position =transform*vec4(position,1);
 	color = vertexColor;
 }
--- gloom/src/program.cpp
+++ gloom/src/program.cpp
@@ -2,6 +2,14 @@
 #include "program.hpp"
 #include "gloom/gloom.hpp"
 #include "gloom/shader.hpp"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+//Camera
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
 GLuint createVAO(GLfloat* vertices, GLfloat* RGBA, int size_vertices,int size_index,int size_RGB,GLuint* indices) {
 	GLuint array;
 	GLuint VBO_vertices;
@@ -42,55 +50,6 @@ GLuint createVAO(GLfloat* vertices, GLfloat* RGBA, int size_vertices,int size_in
 void runProgram(GLFWwindow* window)
 {
 
-	Gloom::Shader shader;shader.makeBasicShader("../gloom/shaders/simple.vert","../gloom/shaders/simple.frag");
-	GLfloat RGBA[] = { 
-		1,0,0,1,
-		0,1,0,1,
-		0,0,1,1,
-
-		1,0,0,1,
-		0,1,0,1,
-		0,0,1,1,
-		
-		1,0,0,1,
-		0,1,0,1,
-		0,0,1,1,
-		
-		1,0,0,1,
-		0,1,0,1,
-		0,0,1,1,
-
-		1,0,0,1,
-		0,1,0,1,
-		0,0,1,1,
-
-	};
-	//All 5 triangles
-	GLfloat vertices1[] = { -0.6, -0.6, 0, -0.3, -0.6, 0, -0.3, 0,0,
-							0.6, 0.6, 0, 0, 0.6, 0, 0, 0, 0 ,
-							-0.8, 0.3, 0, -0.2, 0.3, 0, -0.8, 0.8, 0,  
-							-0.6, 0, 0, -0.6, -0.3, 0, -0.5, 0,0,
-						-0.9, 0, 0, -0.9, -0.3, 0, -0.7, 0,0 };
-
-	//1 Triangle for task 1c and 2d)
-	GLfloat vertices[] = {	
-							-0.3, 0, 0,
-							-0.6, -0.6, 0,
-							-0.3, -0.6, 0
-		 };
-	//Triangle for task 1d)
-	GLfloat vertice2[] = {
-		0.6, -0.8, -1.2,
-		0, 0.4, 0,
-		-0.8, -0.2, 1.2
-	};
-	//indices for all 5 triangles
-	GLuint indices1[] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14};
-	//Indices for task 1c , 2d,c)
-	GLuint indices[] = { 0,1,2};
-	//Creat the VAO with vertices and with the sizes for both the buffers and the indices. Here sizes are given to make 5 triangles. 
-	GLuint vao = createVAO(vertices,RGBA, 9*5,15,4*15,indices);
-	printGLError();
 
     // Set GLFW callback mechanism(s)
     glfwSetKeyCallback(window, keyboardCallback);
@@ -107,6 +66,60 @@ void runProgram(GLFWwindow* window)
 
     // Set up your scene here (create Vertex Array Objects, etc.)
 
+	Gloom::Shader shader; shader.makeBasicShader("../gloom/shaders/simple.vert", "../gloom/shaders/simple.frag");
+	GLfloat RGBA[] = {
+		1,0,0,1,
+		0,1,0,1,
+		0,0,1,1,
+
+		1,0,0,1,
+		0,1,0,1,
+		0,0,1,1,
+
+		1,0,0,1,
+		0,1,0,1,
+		0,0,1,1,
+
+		1,0,0,1,
+		0,1,0,1,
+		0,0,1,1,
+
+		1,0,0,1,
+		0,1,0,1,
+		0,0,1,1,
+
+	};
+	//All 5 triangles
+	GLfloat vertices1[] = { -0.6, -0.6, 0, -0.3, -0.6, 0, -0.3, 0,0,
+		0.6, 0.6, 0, 0, 0.6, 0, 0, 0, 0 ,
+		-0.8, 0.3, 0, -0.2, 0.3, 0, -0.8, 0.8, 0,
+		-0.6, 0, 0, -0.6, -0.3, 0, -0.5, 0,0,
+		-0.9, 0, 0, -0.9, -0.3, 0, -0.7, 0,0 };
+
+	//1 Triangle for task 1c and 2d)
+	GLfloat vertices[] = {
+		-0.3, 0, 0,
+		-0.6, -0.6, 0,
+		-0.3, -0.6, 0
+	};
+	//Triangle for task 1d)
+	GLfloat vertice2[] = {
+		0.6, -0.8, -1.2,
+		0, 0.4, 0,
+		-0.8, -0.2, 1.2
+	};
+	//indices for all 5 triangles
+	GLuint indices1[] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,14 };
+	//Indices for task 1c , 2d,c)
+	GLuint indices[] = { 0,1,2 };
+	//Creat the VAO with vertices and with the sizes for both the buffers and the indices. Here sizes are given to make 5 triangles. 
+	GLuint vao = createVAO(vertices, RGBA, 9 * 5, 15, 4 * 15, indices);
+	printGLError();
+
+
+
+
+
     // Rendering Loop
     while (!glfwWindowShouldClose(window))
     {
@@ -115,10 +128,23 @@ void runProgram(GLFWwindow* window)
 
 
         // Draw your scene here
-		// Activate shader program
-		glBindVertexArray(vao);
 
+
+		glBindVertexArray(vao);
+		// Activate shader program
 		shader.activate();
+
+		//Uniform Transformation
+		glm::mat4x4 IDmatrix(1.0);
+		glUniformMatrix4fv(3, 1, GL_FALSE, glm::value_ptr(IDmatrix));
+
+
+		//Transform to camera coordinate system
+		glm::mat4 view;
+		view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+
+
+
 		glDrawElements(GL_TRIANGLES, 15, GL_UNSIGNED_INT, 0);
 	
 		glBindVertexArray(0);
@@ -137,6 +163,9 @@ void runProgram(GLFWwindow* window)
 }
 
 
+
+
+
 void keyboardCallback(GLFWwindow* window, int key, int scancode,
                       int action, int mods)
 {
@@ -145,4 +174,50 @@ void keyboardCallback(GLFWwindow* window, int key, int scancode,
     {
         glfwSetWindowShouldClose(window, GL_TRUE);
     }
+
+
+	//Move left
+	if (key == GLFW_KEY_LEFT && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+
+	}   
+	//Move right
+	if (key == GLFW_KEY_RIGHT && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}  
+	//Move up
+	if (key == GLFW_KEY_UP && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}   
+	//Move down
+	if (key == GLFW_KEY_DOWN && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
+	//Move forward
+	if (key == GLFW_KEY_LEFT_SHIFT && key == GLFW_KEY_UP && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}   
+	//Move backward
+	if (key == GLFW_KEY_LEFT_SHIFT && key == GLFW_KEY_DOWN && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}   
+
+	//Rotate horizontally
+	if ((key == GLFW_KEY_LEFT_SHIFT && (key == GLFW_KEY_LEFT || key == GLFW_KEY_RIGHT)) && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+
+	//Rotate vertically
+	if ((key == GLFW_KEY_LEFT_SHIFT && (key == GLFW_KEY_UP || key == GLFW_KEY_DOWN)) && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
 }
